a3c81ae54a551d2f4bd0b4bc62914dceb5924903
hewep <Administrator@x6x8-20140519VA>
2014-08-25 21:27:05 +0800

    权限管理--初始

--- WebRoot/res/admin/app.js
+++ WebRoot/res/admin/app.js
@@ -5,9 +5,10 @@ define([
         'controllers/category',
         'controllers/user',
         'controllers/role',
+        'controllers/menu',
         'common/directives',
         'common/services'
-],function(angular, category, user, role){
+],function(angular, category, user, role,menu){
 	var app =  angular.module('adminApp', ['ngRoute','CommDirectives','CommServices']);
 	
 		app.config(['$routeProvider','$locationProvider',  function($routeProvider, $locationProvider) {
@@ -15,7 +16,11 @@ define([
 			  				.when('/component_icons', {templateUrl:'core/icons.html', controller: null})
 			  				.when('/user_list', {templateUrl:'core/admin/sys/user_list.html', controller: user.list})
 			  				.when('/user_info', {templateUrl:'core/admin/sys/user_info.html', controller: user.info})
+			  				.when('/user_grant', {templateUrl:'core/admin/sys/user_grant.html', controller: user.grant})
 			  				.when('/role_list', {templateUrl:'core/admin/sys/role_list.html', controller: role.list})
+//			  				.when('/role_grant', {templateUrl:'core/admin/sys/role_grant.html', controller: role.grant})
+			  				.when('/menu_list', {templateUrl:'core/admin/sys/menu_list.html', controller: menu.list})
+			  				.when('/menu_info', {templateUrl:'core/admin/sys/menu_info.html', controller: menu.info})
 			  				.otherwise({redirectTo: '/'});
 			  				
 			  	// configure html5		
